a91135da721d807339846bce54631cfa05ddd11c
a0988591269 <53168882+a0988591269@users.noreply.github.com>
2019-11-28 15:30:03 +0800

    Travel

--- HolyGo/ViewModels/TravelViewModel.cs
+++ HolyGo/ViewModels/TravelViewModel.cs
@@ -16,9 +16,13 @@ namespace HolyGo.ViewModels
         /// </summary>
         public string Title { get; set; }
         /// <summary>
+        /// 行程內容
+        /// </summary>
+        public string Contents { get; set; }
+        /// <summary>
         /// 行程時長
         /// </summary>
-        public DateTime Time { get; set; }
+        public int Time { get; set; }
         /// <summary>
         /// 行程國家
         /// </summary>
@@ -31,10 +35,9 @@ namespace HolyGo.ViewModels
         /// 行程圖片
         /// </summary>
         public string Images { get; set; }
-        public string Explain { get; set; }
         /// <summary>
-        /// 行程價錢
+        /// 行程說明
         /// </summary>
-        public int Cost { get; set; }
+        public string Explain { get; set; }
     }
 }
