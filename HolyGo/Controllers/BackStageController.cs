ecb982030562fdd8c4fd3b4fa075616e28493e5b
a0988591269 <53168882+a0988591269@users.noreply.github.com>
2019-12-05 20:55:51 +0800

    BackStage

---
+++ HolyGo/Controllers/BackStageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HolyGo.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class BackStageController : Controller
+    {
+        // GET: BackStage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Travel()
+        {
+            return View();
+        }
+        public ActionResult Ticket()
+        {
+            return View();
+        }
+        public ActionResult Guide()
+        {
+            return View();
+        }
+    }
+}
