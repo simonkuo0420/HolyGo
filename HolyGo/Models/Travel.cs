8f9a31ed7b2be3edcd258fb0ea3ffe845847f9d6
a0988591269 <53168882+a0988591269@users.noreply.github.com>
2019-12-05 23:24:54 +0800

    BackStageRepository

--- HolyGo/Models/Travel.cs
+++ HolyGo/Models/Travel.cs
@@ -5,6 +5,7 @@ namespace HolyGo.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Web.Mvc;
 
     [Table("Travel")]
     public partial class Travel
@@ -44,6 +45,9 @@ namespace HolyGo.Models
         [Required]
         [StringLength(3)]
         public string Status { get; set; }
+
+        [AllowHtml]
+        public string Explain { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Favorite> Favorite { get; set; }
---
+++ HolyGo/Repository/BackStageRepository.cs
@@ -0,0 +1,36 @@
+﻿using HolyGo.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace HolyGo.Repository
+{
+    public class BackStageRepository
+    {
+        #region 建構式
+        public static string connString;
+        public SqlConnection conn;
+
+        public BackStageRepository()
+        {
+            if (string.IsNullOrEmpty(connString))
+            {
+                connString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            }
+            if(conn == null)
+            {
+                conn = new SqlConnection(connString);
+            }
+        }
+        #endregion
+
+        public string CreateTravel(Travel travel)
+        {
+
+        }
+
+    }
+}
